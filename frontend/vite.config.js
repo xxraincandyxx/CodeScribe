b4334734a10434eafa497126af85c111fef96216
ra1n <xxraincandyxx@gmail.com>
2025-08-27 04:55:58 +0800

    chore(build): update config base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'code-scribe',
+  base: './',
   server: {
     host: '0.0.0.0',
     port: 5173,
